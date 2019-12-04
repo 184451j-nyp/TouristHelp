59f4197afb41f656590284a3eac19093c7b69d15
Timothy John Low <44186494+184451j-nyp@users.noreply.github.com>
2019-12-04 15:54:51 +0800

    added bootstrap

---
+++ TouristHelp/Models/Direction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TouristHelp.Models
+{
+    public class Direction
+    {
+        public string id { get; }
+        public string user { get; set; }
+        public string nextDir { get; set; }
+        public string origin { get; set; }
+        public string destination { get; set; }
+        public int group { get; set; }
+
+        public Direction(string id, string user, string nextDir, string origin, string destination, int group)
+        {
+            this.id = id;
+            this.user = user;
+            this.nextDir = nextDir;
+            this.origin = origin;
+            this.destination = destination;
+            this.group = group;
+        }
+    }
+}
--- TouristHelp/Models/User.cs
+++ TouristHelp/Models/User.cs
@@ -7,6 +7,41 @@ namespace TouristHelp.Models
 {
     public class User
     {
+        public string userId { get; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
 
+        public User(string id, string name, string email, string pswd)
+        {
+            userId = id;
+            this.name = name;
+            this.email = email;
+            password = pswd;
+        }
+    }
+
+    public class TourGuide : User
+    {
+        public double rating { get; set; }
+        public string description { get; set; }
+        public List<string> languages { get; set; }
+
+        public TourGuide(string id, string name, string email, string pswd, double rating, string description, List<string> languages):base(id, name, email, pswd)
+        {
+            this.rating = rating;
+            this.description = description;
+            this.languages = languages;
+        }
+    }
+
+    public class Tourist : User
+    {
+        public string nationality { get; set; }
+
+        public Tourist(string id, string name, string email, string pswd, string nationality):base(id, name, email, pswd)
+        {
+            this.nationality = nationality;
+        }
     }
 }
