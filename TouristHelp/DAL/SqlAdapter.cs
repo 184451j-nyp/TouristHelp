f240ba30288b9b93a976d3e31b930aeefee1edf0
Timothy John Low <44186494+184451j-nyp@users.noreply.github.com>
2020-01-16 02:22:54 +0800

    voucher table + some DAO

--- TouristHelp/DAL/SqlAdapter.cs
+++ TouristHelp/DAL/SqlAdapter.cs
@@ -9,7 +9,7 @@ using TouristHelp.Models;
 
 namespace TouristHelp.DAL
 {
-    public class SqlQuery
+    public class SqlHelper
     {
         public DataSet Query(string sqlStmt)
         {
@@ -22,26 +22,6 @@ namespace TouristHelp.DAL
             da.Fill(ds);
 
             return ds;
-        }
-    }
-
-    public class SqlInsert
-    {
-        string DBConnect = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
-        public void insertUser(Tourist user)
-        {
-            SqlConnection myConn = new SqlConnection(DBConnect);
-            string sqlstmt = "Insert into Tourists (password, name, email, nationality) Values (@paraPswd, @paraName, @paraEmail, @paraNation)";
-            SqlCommand sqlCmd = new SqlCommand(sqlstmt, myConn);
-
-            sqlCmd.Parameters.AddWithValue("@paraPswd", user.password);
-            sqlCmd.Parameters.AddWithValue("@paraName", user.name);
-            sqlCmd.Parameters.AddWithValue("@paraEmail", user.email);
-            sqlCmd.Parameters.AddWithValue("@paraNation", )
-        }
-        public void insertDirection(Direction direction)
-        {
-
         }
     }
 }
--- TouristHelp/DAL/UserDAO.cs
+++ TouristHelp/DAL/UserDAO.cs
@@ -1,11 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
+using TouristHelp.Models;
 
 namespace TouristHelp.DAL
 {
     public class UserDAO
     {
+        private SqlHelper helper = new SqlHelper();
+
+        public List<Tourist> SelectAllTourists()
+        {
+            DataSet ds = helper.Query("Select Tourists.tourist_id, Users.name, Users.password, Users.email, Tourists.nationality" +
+                "From Tourists " +
+                "Inner Join Users On Tourists.user_id = Users.user_id");
+            
+            List<Tourist> userList = new List<Tourist>();
+            int rec_cnt = ds.Tables[0].Rows.Count;
+            for (int i = 0; i < rec_cnt; i++)
+            {
+                DataRow row = ds.Tables[0].Rows[i];
+                int id = int.Parse(row["tourist_id"].ToString());
+                string name = row["name"].ToString();
+                string password = row["password"].ToString();
+                string email = row["email"].ToString();
+                string nationality = row["nationality"].ToString();
+                Tourist obj = new Tourist(id, name, email, password, nationality);
+                userList.Add(obj);
+            }
+            return userList;
+        }
+
+        public Tourist SelectTourist(int user_id)
+        {
+            DataSet ds = helper.Query("Selec")
+        }
+
+        private List<TourGuide> SelectAllTourGuides()
+        {
+            DataSet ds = helper.Query("Select TourGuides.tourguide_id, Users.name, Users.password, Users.email, TourGuides.rating, TourGuides.desc, TourGuides.languages " +
+                "From TourGuides " +
+                "Inner Join Users On TourGuides.user_id = Users.user_id");
+
+            List<TourGuide> userList = new List<TourGuide>();
+            int rec_cnt = ds.Tables[0].Rows.Count;
+            for (int i = 0; i < rec_cnt; i++)
+            {
+                DataRow row = ds.Tables[0].Rows[i];
+                int id = int.Parse(row["tourguide_id"].ToString());
+                string name = row["name"].ToString();
+                string password = row["password"].ToString();
+                string email = row["email"].ToString();
+                double rating = double.Parse(row["rating"].ToString());
+                string desc = row["desc"].ToString();
+                string languages = row["languages"].ToString();
+                TourGuide obj = new TourGuide(id, name, email, password, rating, desc, languages);
+                userList.Add(obj);
+            }
+            return userList;
+        }
     }
 }
