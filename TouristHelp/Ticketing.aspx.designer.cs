9b65015a1e2e043636dfef4346ff3694fe024c8a
181196T@mymail.nyp.edu.sg <181196T@mymail.nyp.edu.sg>
2020-02-10 22:50:30 +0800

    Ticket slight fix

--- TouristHelp/Ticketing.aspx.designer.cs
+++ TouristHelp/Ticketing.aspx.designer.cs
@@ -85,6 +85,15 @@ namespace TouristHelp {
         protected global::AjaxControlToolkit.PopupControlExtender tbDate_PopupControlExtender;
         
         /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
